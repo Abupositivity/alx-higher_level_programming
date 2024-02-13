7b2435a77311bff0eb19aa9ebf306e5094cbc4f5
Abupositivity <abupositivity@gmail.com>
2024-02-12 17:02:40 -0800

    task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Contains a write_file function
+"""
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and returns the number of characters written"""
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
+        return len(text)
