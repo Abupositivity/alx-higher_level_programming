cee61d4e648075453e950dae5049ddef117f7ae9
Abupositivity <abupositivity@gmail.com>
2024-02-12 18:51:53 -0800

    task 10 corrections

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
 """
-Contains the clas "Student"
+Defines a class Student
 """
 
 
 class Student:
-    """Representation of a student"""
+    """Represent a student"""
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """Initializes a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """returns a dictionary representation of the Student instance.
-        If attrs is a list of strings,
-        only attribute names contained in this list must be retrieved.
+        """Returns a dictionary representation of the Student instance.
+        If attrs is a list of strings represent only those attributes
+        included in the list.
         Otherwise, all attributes must be retrieved
         Args:
         attrs(list): (Optional) The attributes  to represent """
