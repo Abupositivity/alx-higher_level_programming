369dda1272c2e44d5d9ca3c6420443f95d546a2f
Abupositivity <abupositivity@gmail.com>
2024-02-12 18:53:20 -0800

    task 10 corrections

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""
-Defines a class Student
-"""
+"""Defines a class Student."""
 
 
 class Student:
-    """Represent a student"""
+    """Represent a student."""
+
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student.
+        """Initialize a new Student.
         Args:
             first_name (str): The first name of the student.
             last_name (str): The last name of the student.
@@ -17,14 +16,14 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Returns a dictionary representation of the Student instance.
-        If attrs is a list of strings represent only those attributes
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
         included in the list.
-        Otherwise, all attributes must be retrieved
         Args:
-        attrs(list): (Optional) The attributes  to represent """
-    if (type(attrs) == list and
-            all(type(ele) == str for ele in attrs)):
-        return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
-    return self.__dict__
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
