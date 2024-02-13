cc88c9f3d8eaa51797c56e6b73456c5369b439cd
Abupositivity <abupositivity@gmail.com>
2024-02-12 17:06:08 -0800

    task 1 pycode correction

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,8 +3,9 @@
 Contains a write_file function
 """
 
+
 def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8) and returns the number of characters written"""
+    """writes a string to a text file (UTF8) & returns the no of characters"""
     with open(filename, "w", encoding="utf-8") as file:
         file.write(text)
         return len(text)
