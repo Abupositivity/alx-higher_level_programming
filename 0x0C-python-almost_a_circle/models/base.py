6077499a5545370a821cabba9d73934d8d68fdaa
Abupositivity <abupositivity@gmail.com>
2024-02-19 19:18:58 -0800

    file to instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Defines a base class model class."""
 import json
-import csv
-import turtle
 
 
 class Base:
@@ -64,7 +62,7 @@ class Base:
 
     @classmethod
     def create(cls, **dictionary):
-    """Return a class instantied from a dictionary of attributes.
+        """Return a class instantied from a dictionary of attributes.
         Args:
             **dictionary (dict): Key/value pairs of attributes to initialize.
         """
@@ -75,3 +73,19 @@ class Base:
                 new = cls(1)
             new.update(**dictionary)
             return new
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of classes instantiated from a file of JSON strings.
+        Reads from `<cls.__name__>.json`.
+        Returns:
+            If the file does not exist - an empty list.
+            Otherwise - a list of instantiated classes.
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
