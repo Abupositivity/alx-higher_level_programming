4d2ee2c96d07c43bd5308f57f34319a53628249d
Abupositivity <abupositivity@gmail.com>
2024-03-19 11:58:25 -0700

    creates database with table in MySQL

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates the database hbtn_0d_usa and the table states in MySQL.
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`STATES` (
+       PRIMARY KEY(`id`),
+       `id` INT NOT NULL AUTO_INCREMENT,
+       `name` VARCHAR(256) NOT NULL
+       );
