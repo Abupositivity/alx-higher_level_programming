29c72960aedee3b13eae628a49e7cef8fbc20f35
Abupositivity <abupositivity@gmail.com>
2024-04-21 21:14:54 -0700

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 """Contains the list_states Module"""
-import sys
 import MySQLdb
+import sys
 
 
 def list_states(username, password, name):
     """Function that lists all the states from the database"""
-    
+
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -15,13 +15,13 @@ def list_states(username, password, name):
         password=password,
         db=name
     )
-    
+
     cursor = conn.cursor()
     query = "SELECT * FROM states ORDER BY states.id ASC"
 
-    cusor.execute(query)
+    cursor.execute(query)
     states = cursor.fetchall()
-    
+
     for state in states:
         print(state)
 
@@ -34,5 +34,5 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     name = sys.argv[3]
-    
-    list_states(username, password, database)
+
+    list_states(username, password, name)
