4fc6e9704a56e2c47112c6f198cdd8594b3fd0bd
Abupositivity <abupositivity@gmail.com>
2024-05-02 00:30:04 -0700

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+"""Displays the X-Request-Id header variable of a request to a given URL.
+Usage: ./5-hbtn_header.py <URL>
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = request.get(url)
+    print(r.headers.get("X-Request-Id"))
